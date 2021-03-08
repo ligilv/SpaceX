d0c22c1ef2ba4906f1a00c61609663d81ed2672d
Ligil <46999151+ligilv@users.noreply.github.com>
2021-03-08 10:19:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import ReactModal from "react-modal";
+
+import "./App.css";
+
+import Dashboard from "./Components/Dashboard";
+ReactModal.setAppElement("#root ");
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
